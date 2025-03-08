19bfc41e7429799aee01e0a96494657438d07e8f
Francisco_Dougllas <franciscodouglas77@outlook.com>
2025-03-08 14:08:54 -0300

    -------

---
+++ 01_Fundamentos/arrays.js
@@ -0,0 +1,12 @@
+const frutas = ['banana', 'maçã', 'laranja', 'uva', 'melancia'];
+
+// Acessando um elemento do array
+console.log(frutas[0]); // banana
+
+// Alterando um elemento do array
+frutas[0] = 'morango';
+console.log(frutas[0]); // morango
+
+// Adicionando um elemento ao array
+frutas.push('abacaxi');
+console.log(frutas); // ['morango', 'maçã', 'laranja', 'uva', 'melancia', 'abacaxi']
